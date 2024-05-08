04192579ede138ed2b7ea7547c0bac349942dc98
Luiz Cavina <79873070+Pizzy23@users.noreply.github.com>
2024-05-08 14:49:35 -0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,7 @@ import (
 // @version         1.0
 // @description     This is a server for app.
 
-// @host      52.15.128.117:8080
+// @host      3.144.137.223:8080
 
 // @securityDefinitions.basic  BasicAuth
 
